4f99e9fe4838ba8bc0e7cf9851016bca3a3239f3
David Zheng <dzhengx@gmail.com>
2017-09-07 15:29:46 -0700

    Add load rom functionality to cpu

--- chippy/chippy.py
+++ chippy/chippy.py
@@ -1,7 +1,8 @@
+import sys
 from cpu import CPU
 
 if __name__ == '__main__':
     chip8 = CPU() 
-
+    chip8.load_rom(sys.argv[1])
 
 
--- chippy/cpu.py
+++ chippy/cpu.py
@@ -1,3 +1,5 @@
+import os
+
 class CPU():
     def __init__(self):
         self.memory = [None] * 4096
@@ -5,8 +7,33 @@ class CPU():
         self.address = [None] * 16
         self.stack = [None] * 16
         self.key = [None] * 16
-        self.display_pixels = [[0 for _ in range(65)] for _ in range(33)] 
+        self.display_pixels = [[0 for _ in range(64)] for _ in range(32)] 
         self.pc = 0x200
         self.sp = 0
         self.delay_timer = 0
         self.sound_timer = 0
+    
+    def load_rom(self, rom_name):
+        print("Loading %s..." % (rom_name))
+        os.chdir('..')
+        os.chdir('roms')
+        try:
+            rom = open(rom_name, "rb")
+        except IOError:
+            print("Rom does not exist, please enter a valid rom file.")
+            sys.exit()
+        else:
+            rom_bytes = rom.read()
+            if len(rom_bytes) > (4096 - 512):
+                print("Rom file is too large, please choose a valid rom file.")
+            #print(len(rom_string))
+            for byte in range(0, len(rom_bytes)):
+                self.memory[byte + 512] = rom_bytes[byte]
+
+            #print(rom_bytes[0] << 8 | rom_bytes[1])
+            print("Done loading %s!" %(rom_name))
+            rom.close()
+
+
+
+
