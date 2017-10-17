f91d91009e3780fc377ab2389fa01a73fc0ef9fb
David Zheng <dzhengx@gmail.com>
2017-10-17 13:14:00 -0700

    Fix bitwise operation precedence bug

--- chippy/chippy.py
+++ chippy/chippy.py
@@ -8,8 +8,14 @@ if __name__ == '__main__':
     chip8 = CPU(chip8_display) 
     chip8.load_rom(sys.argv[1])
     running = True
+    pygame.time.set_timer(pygame.USEREVENT + 1, 60)
+    
     while(running):
+        pygame.time.wait(1)
         chip8.perform_cycle()
+        for event in pygame.event.get():
+            if event.type == pygame.USEREVENT + 1:
+                chip8.timer_decrement()
         if chip8.memory[chip8.pc] == None:
            sys.exit()
 
--- chippy/cpu.py
+++ chippy/cpu.py
@@ -48,7 +48,15 @@ class CPU(object):
 
         for x in range(0, len(self.font_set)):
             self.memory[x] = self.font_set[x]
-    
+
+    def testing(self):
+        for num in range (0, len(self.registers)):
+            print("V" + str(num) + ": " +  str(self.registers[num]))
+        print("I: " + str(self.register_I))
+        print("pc: " + str(self.pc))
+        print("sp: " + str(self.sp))
+        print("dt: " + str(self.delay_timer))
+        print("st: " + str(self.sound_timer))
     def load_rom(self, rom_name):
         """
         Checks if the user entered rom name exists in the proper directory. If the rom exists
@@ -73,6 +81,12 @@ class CPU(object):
                 self.memory[byte + self.pc] = rom_bytes[byte]
             print("Done loading %s!" %(rom_name))
             rom.close()
+
+    def timer_decrement(self):
+        if self.delay_timer != 0:
+            self.delay_timer -= 1
+        if self.sound_timer != 0:
+            self.sound_timer -= 1
 
     def get_opcode(self):
         """      
@@ -102,16 +116,18 @@ class CPU(object):
                 self.pc += 2 
             # Opcode 00EE: returns from subroutine
             elif last_hex == 0x000E: 
-                self.sp -= 1
+                #self.sp -= 1
                 self.pc = self.stack[self.sp]
+                self.sp -= 1
                 self.pc += 2
 
         # Opcode 1NNN: Jump to address NNN        
         elif first_hex == 0x1000:
             # Get last 3 hex values
             address = opcode & 0x0FFF
-            self.pc = address 
+            self.pc = address
 
+        # TODO check implementation
         # Opcode 2NNN: Call subroutine at NNN
         # Adds current pc to stack and increments sp 
         elif first_hex == 0x2000:
@@ -119,102 +135,101 @@ class CPU(object):
             self.stack[self.sp] = self.pc
             self.sp += 1
             self.pc = address
-
         # Opcode 3XKK: Skips next instruction if value stored in register X = KK
         elif first_hex == 0x3000:
-            if (self.registers[opcode & 0x0F00 >> 8] == (opcode & 0x00FF)):
+            if (self.registers[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF)):
                 self.pc += 4
             else:
                 self.pc += 2
         
         # Opcode 4XKK: Skips next instruction if value stored in register X != KK
         elif first_hex == 0x4000:
-            if (self.registers[opcode & 0x0F00 >> 8] != (opcode & 0x00FF)):
+            if (self.registers[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)):
                 self.pc += 4
             else:
                 self.pc += 2
 
         # Opcode 5XY0: Skips next instruction if value stored in register X = value in register Y    
         elif first_hex == 0x5000:
-            if (self.registers[opcode & 0x0F00 >> 8] == self.registers[opcode & 0x00F0 >> 4]):
+            if (self.registers[(opcode & 0x0F00) >> 8] == self.registers[(opcode & 0x00F0) >> 4]):
                 self.pc += 4
             else:
                 self.pc += 2
 
         # Opcode 6XKK: Load KK into register X 
         elif first_hex == 0x6000:
-            value = opcode & 0x00FF 
-            self.registers[opcode & 0x0F00 >> 8] = value
+            value = opcode & 0x00FF
+            self.registers[(opcode & 0x0F00) >> 8] = value
             self.pc += 2
 
         # Opcode 7XKK: Adds KK to the value in register X and stores it in register X
         elif first_hex == 0x7000:
-            self.registers[opcode & 0x0F00 >> 8] += (opcode & 0x00FF)
+            self.registers[(opcode & 0x0F00) >> 8] += (opcode & 0x00FF)
             self.pc += 2
 
         elif first_hex == 0x8000:
             last_hex = opcode & 0x000F
             # Opcode 8XY0: Set value of register X to the value of register Y
             if last_hex == 0x000:
-                self.registers[opcode & 0x0F00 >> 8] = self.registers[opcode & 0x00F0 >> 4]
+                self.registers[(opcode & 0x0F00) >> 8] = self.registers[(opcode & 0x00F0) >> 4]
                 self.pc += 2
             # Opcode 8XY1: Set value of register X to (value of register X OR value of register Y)
             elif last_hex == 0x001:
-                self.registers[opcode & 0x0F00 >> 8] |= self.registers[opcode & 0x00F0 >> 4]
+                self.registers[(opcode & 0x0F00) >> 8] |= self.registers[(opcode & 0x00F0) >> 4]
                 self.pc += 2 
             # Opcode 8XY2: Set value of register X to (value of register X AND value of register Y)
             elif last_hex == 0x002:
-                self.registers[opcode & 0x0F00 >> 8] &= self.registers[opcode & 0x00F0 >> 4]
+                self.registers[(opcode & 0x0F00) >> 8] &= self.registers[(opcode & 0x00F0) >> 4]
                 self.pc += 2
             # Opcode 8XY3: Set value of register X to (value of register X XOR value of register Y)
             elif last_hex == 0x003:
-                self.registers[opcode & 0x0F00 >> 8] ^= self.registers[opcode & 0x00F0 >> 4]
+                self.registers[(opcode & 0x0F00) >> 8] ^= self.registers[(opcode & 0x00F0) >> 4]
                 self.pc += 2
             # Opcode 8XY4: Set value of register X to (value of register X ADD value of register Y) and set carry
             elif last_hex == 0x004:
-                value_sum  = self.registers[opcode & 0x0F00 >> 8] + self.registers[opcode & 0x00F0 >> 4]
+                value_sum  = self.registers[(opcode & 0x0F00) >> 8] + self.registers[(opcode & 0x00F0) >> 4]
                 # Only keeps the lowest 8 bits if the sum is greater than 0xFF and sets the carry register to 1
                 if value_sum > 0xFF:
                     self.registers[0xF] = 1
-                    self.registers[opcode & 0x0F00 >> 8] = (value_sum & 0x00FF)
+                    self.registers[(opcode & 0x0F00) >> 8] = (value_sum & 0x00FF)
                 else:
                     self.registers[0xF] =0
-                    self.registers[opcode & 0x0F00 >> 8] = value_sum
+                    self.registers[(opcode & 0x0F00) >> 8] = value_sum
                 self.pc += 2
             # Opcode 8XY5: Set value of register X to (value of register X SUB value of register Y)
             elif last_hex == 0x005:
                 # Sets carry register to 0 if there is a borrow else set to 1
-                if (self.registers[opcode & 0x0F00 >> 8] > self.registers[opcode & 0x00F0 >> 4]):
+                if (self.registers[(opcode & 0x0F00) >> 8] > self.registers[(opcode & 0x00F0) >> 4]):
                     self.registers[0xF] = 1
                 else: 
                     self.registers[0xF] = 0
-                self.registers[opcode & 0x0F00 >>8] -= self.registers[opcode & 0x00F0 >>4]
+                self.registers[(opcode & 0x0F00) >> 8] -= self.registers[(opcode & 0x00F0) >> 4]
                 self.pc += 2
             # Opcode 8XY6: Right shift the value of register X by 1
             elif last_hex == 0x006:
                 # Keeps the least significant bit of the value of register X in register F
-                self.registers[0xF] = (self.registers[opcode & 0x0F00 >> 8] & 0x0001)
-                self.registers[opcode & 0x0F00 >> 8] = (self.registers[opcode & 0x0F00 >> 8] >> 1)
+                self.registers[0xF] = (self.registers[(opcode & 0x0F00) >> 8] & 0x0001)
+                self.registers[(opcode & 0x0F00) >> 8] = (self.registers[(opcode & 0x0F00) >> 8] >> 1)
                 self.pc += 2
             # Opcode 8XY7: Set value of register X to (value of register Y SUB value of register X)
             elif last_hex == 0x007:
                 # Sets carry register to 0 if there is a borrow else set to 1
-                if (self.registers[opcode & 0x0F00 >> 8] < self.registers[opcode & 0x00F0 >> 4]):
+                if (self.registers[(opcode & 0x0F00) >> 8] < self.registers[(opcode & 0x00F0) >> 4]):
                     self.registers[0xF] = 1
                 else:
                     self.registers[0xF] = 0
-                self.registers[opcode & 0x0F00 >> 8] = self.registers[opcode & 0x00F0 >> 4] - self.registers[opcode & 0x0F00 >> 8]
+                self.registers[(opcode & 0x0F00) >> 8] = self.registers[(opcode & 0x00F0) >> 4] - self.registers[(opcode & 0x0F00) >> 8]
                 self.pc += 2
             # Opcode 8XYE: Left shift the value of register X by 1
             elif last_hex == 0x00E:
                 # Keeps the most significant bit of the value of register X in register F
-                self.registers[0xF] = (self.registers[opcode & 0x0F00 >> 8] >> 7)
-                self.registers[opcode & 0x0F00 >> 8] = (self.registers[opcode & 0x0F00 >> 8] << 1)
+                self.registers[0xF] = (self.registers[(opcode & 0x0F00) >> 8] >> 7)
+                self.registers[(opcode & 0x0F00) >> 8] = (self.registers[(opcode & 0x0F00) >> 8] << 1)
                 self.pc += 2
 
         # Opcode 9XY0: Skip next instruction if value of register X != value of register Y
         elif first_hex == 0x9000:
-            if self.registers[opcode & 0x0F00 >> 8] != self.registers[opcode & 0x00F0 >> 4]:
+            if self.registers[(opcode & 0x0F00) >> 8] != self.registers[(opcode & 0x00F0) >> 4]:
                 self.pc += 4
             else:
                 self.pc += 2
@@ -231,7 +246,7 @@ class CPU(object):
         # Opcode CXKK: Sets the value of register X to (random byte AND KK)
         elif first_hex == 0xC000:
             random_byte = randint(0, 255) 
-            self.registers[opcode & 0x0F00 >> 8] = (random_byte & (opcode & 0x00FF))
+            self.registers[(opcode & 0x0F00) >> 8] = (random_byte & (opcode & 0x00FF))
             self.pc += 2
 
         # Opcode DXYN: Display an N-byte sprite starting at memory location I at (value of register X, value of register Y)
@@ -244,7 +259,6 @@ class CPU(object):
             self.registers[0x000F] = 0
             
             for y_val in range(n):
-                print(self.register_I + y_val)
                 sprite_byte = self.memory[self.register_I + y_val]
                 sprite_byte = bin(sprite_byte)[2:]
                 sprite_byte = sprite_byte.zfill(8)
@@ -268,15 +282,16 @@ class CPU(object):
          
         elif first_hex == 0xE000:
             last_hex = opcode & 0x000F
+            # TODO implement pygame keys
             # Opcode EX9E: Skips the next instruction if key with the value of register X is pressed
             if last_hex == 0x000E:
-                if self.keys[opcode & 0x0F00 >> 8] != 0:
+                if self.keys[(opcode & 0x0F00) >> 8] != 0:
                     self.pc += 4
                 else:
                     self.pc += 2
             # Opcode EXA1: Skips the next instruction if key with the value of register X is not pressed
             if last_hex == 0x0001:
-                if self.keys[opcode & 0x0F00 >> 8] == 0:
+                if self.keys[(opcode & 0x0F00) >> 8] == 0:
                     self.pc += 4
                 else:
                     self.pc +=2
@@ -285,37 +300,38 @@ class CPU(object):
             last_hex = opcode & 0x000F
             # Opcode FX07: Set the value of register X to the value of the delay timer
             if last_hex == 0x0007:
-                self.registers[opcode & 0x0F00 >> 8] = self.delay_timer
+                self.registers[(opcode & 0x0F00) >> 8] = self.delay_timer
                 self.pc += 2
+            # TODO implement pygame keys
             # Opcode FX0A: Wait for a key press and stores the value of the pressed key into register X
             if last_hex == 0x000A:
                 key_was_pressed = False
                 while key_was_pressed is not True:
                     for key in range(0, len(self.keys)):
                         if key is not 0:
-                            self.registers[opcode & 0x0F00 >> 8] = key
+                            self.registers[(opcode & 0x0F00) >> 8] = key
                             key_was_pressed = True
                 self.pc += 2
             # Opcode FX15: Set the value of the delay timer to the value of register X
             if last_hex == 0x0005:
-                self.delay_timer = self.registers[opcode & 0x0F00 >> 8] 
+                self.delay_timer = self.registers[(opcode & 0x0F00) >> 8] 
                 self.pc += 2
             # Opcode FX18: Set the value of the sound timer to the value of register X
             if last_hex == 0x0008:
-                self.sound_timer = self.registers[opxode & 0x0F00 >> 8]
+                self.sound_timer = self.registers[(opcode & 0x0F00) >> 8]
                 self.pc += 2
             # Opcode FX1E: Set the value of register I to (value of register I + value of register X)
             if last_hex == 0x000E:
-                self.register_I += self.registers[opcode & 0x0F00 >> 8]
+                self.register_I += self.registers[(opcode & 0x0F00) >> 8]
                 self.pc += 2
             # Opcode FX29: Set value of register I to the location of sprite for the digit of the value of register X
             # Sprites are 5 bytes long so the value of register X must be multiplied by 5
             if last_hex == 0x0009:
-                self.register_I = self.registers[opcode & 0x0F00 >> 8] * 0x000F
+                self.register_I = self.registers[(opcode & 0x0F00) >> 8] * 0x000F
                 self.pc += 2
             # Opcode FX33: Store the binary-coded decimal representation of the value of register X in memory locations I, I+1, and I+2
             if last_hex == 0x0003:
-                value = self.registers[opcode & 0x0F00 >> 8] 
+                value = self.registers[(opcode & 0x0F00) >> 8] 
                 difference = 2
                 while difference >= 0:
                     self.memory[self.register_I + difference] = value % 10
@@ -325,7 +341,7 @@ class CPU(object):
             # Opcode Fx55: Store the values of register 0 through X in memory starting in location of the value of register I
             if last_hex == 0x0005:
                 location = 0
-                end = opcode & 0x0F00 >> 8
+                end = (opcode & 0x0F00) >> 8
                 while location <= end:
                     self.memory[self.register_I + location] = self.registers[location]
                     location += 1
@@ -333,7 +349,7 @@ class CPU(object):
             # Opcode FX65: Load the registers 0 through X with values starting from the address of the value of register I
             if last_hex == 0x0006:
                 location = 0
-                end = opcode & 0x0F00 >> 8
+                end = (opcode & 0x0F00) >> 8
                 while location <= end:
                     self.registers[location] = self.memory[self.regsiter_I + location]
                     location += 1
@@ -344,7 +360,8 @@ class CPU(object):
 
     def perform_cycle(self):
         current_opcode = self.get_opcode()
-        print(current_opcode)
+        print(hex(current_opcode))
+        self.testing()
         self.perform_opcode(current_opcode)
         if self.draw == True:
             self.display.update_display()
